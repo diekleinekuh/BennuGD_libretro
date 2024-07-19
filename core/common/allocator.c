1fc146322781c1750d5b818c2c278e79c0591b30
diekleinekuh <markus.henschel+github@yaak.ai>
2024-07-19 11:57:17 +0200

    win32 support for  wasteful custom allocator that ensures addresses are 32bit

--- core/common/allocator.c
+++ core/common/allocator.c
@@ -1,13 +1,68 @@
 #include "allocator.h"
-#include <sys/mman.h>
-#include <stddef.h>
-#include <string.h>
+#include <assert.h>
 
 struct Header
 {
     size_t allocated_size;
     size_t used_size;
 };
+
+#ifdef _WIN32
+#include <Windows.h>
+#include <Memoryapi.h>
+
+const size_t page_size = 0x1000;
+const char* limit = (const char*)(1ull << 32 );
+
+char* address = (char*)0x1000;
+
+void* bgd_malloc( size_t size )
+{
+    const size_t pages = (size + sizeof(struct Header)+ page_size - 1) / page_size;
+    const size_t allocated_size = pages * page_size;
+
+
+    while (address + size < limit)
+    {
+        void* result = VirtualAlloc(address, allocated_size, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+
+        if (address == result)
+        {
+            struct Header* header = (struct Header*)address;
+            header->allocated_size = allocated_size;
+            header->used_size = size;
+
+            address += allocated_size;
+
+            return ++header;
+        }
+
+        address += page_size;
+    }
+
+    assert(0);
+    return NULL;
+}
+
+void bgd_free( void *p )
+{
+    if (!p)
+    {
+        return;
+    }
+
+    struct Header* header = p;
+    --header;
+
+    BOOL result = VirtualFree(header, 0, MEM_RELEASE);
+    assert(result);
+}
+
+#else
+
+#include <sys/mman.h>
+#include <stddef.h>
+#include <string.h>
 
 void* bgd_malloc( size_t size )
 {
@@ -29,25 +84,6 @@ void* bgd_malloc( size_t size )
     return ++header;
 }
 
-void *bgd_calloc( size_t num, size_t size )
-{
-    return bgd_malloc(num*size);
-}
-
-void *bgd_realloc( void *p, size_t new_size )
-{
-    void* new_ptr = bgd_malloc(new_size);
-    if (p)
-    {
-        struct Header* header = p;
-        --header;
-        size_t copy_size = new_size < header->used_size ? new_size : header->used_size;
-        memcpy(new_ptr, p, copy_size);
-    }
-    
-    return new_ptr;
-}
-
 void bgd_free( void *p )
 {
     if (!p)
@@ -61,11 +97,42 @@ void bgd_free( void *p )
     munmap(header, header->allocated_size);
 }
 
+#endif //_WIN32
+
+void* bgd_realloc(void* p, size_t new_size)
+{
+    if (p)
+    {
+        struct Header* header = p;
+        --header;
+        if (new_size <= header->allocated_size-sizeof(header))
+        {
+            header->used_size = new_size;
+            return p;
+        }
+        else
+        {
+            void* new_ptr = bgd_malloc(new_size);
+            size_t copy_size = new_size < header->used_size ? new_size : header->used_size;
+            memcpy(new_ptr, p, copy_size);
+            return new_ptr;
+        }
+    }
+    else
+    {
+        return bgd_malloc(new_size);
+    }
+}
+
+void* bgd_calloc(size_t num, size_t size)
+{
+    return bgd_malloc(num * size);
+}
+
 char* bgd_strdup(const char* s)
 {
-    size_t size = strlen(s)+1;
-    char * result = (char*)bgd_malloc(size);
+    size_t size = strlen(s) + 1;
+    char* result = (char*)bgd_malloc(size);
     memcpy(result, s, size);
     return result;
 }
-
