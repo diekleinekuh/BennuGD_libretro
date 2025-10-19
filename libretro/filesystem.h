7806cec320b2a1669e17f4394b2406a46b14481b
diekleinekuh <markus.henschel@yaak.ai>
2025-10-19 13:21:44 +0200

    add option to disable case insensitive file io emulation

--- libretro/filesystem.h
+++ libretro/filesystem.h
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include "file/file_path.h"
 
-void init_filesystem(const char * initial_directory, const char * save_dir, struct retro_vfs_interface_info* vfs_info);
+void init_filesystem(const char * initial_directory, const char * save_dir, struct retro_vfs_interface_info* vfs_info, bool in_case_insensitive_io);
 void cleanup_filesystem();
 
 char * get_content_basename();
